66d56c48f2fbef64b375bcdf865d35370b766992
Wasil P <it.dzemidok@gmail.com>
2024-02-29 14:31:42 +0100

    Add file run

---
+++ Education_system/run.py
@@ -0,0 +1,36 @@
+from models import Group
+from models import UserCourseAccess
+
+from django.utils import timezone
+from django.db import transaction
+
+
+def distribute_users_to_groups(user_course_access: UserCourseAccess):
+
+    course = user_course_access.course
+    user = user_course_access.user
+    groups = Group.objects.filter(course=course)
+    filtered_groups = []
+
+    for group in groups:
+        if group.users.count() < group.max_users:
+            filtered_groups.append(group)
+
+    if not filtered_groups:
+        raise ValueError("Достигнуто максимальное количество пользователей во всех группах.")
+
+
+    with transaction.atomic():
+        if course.start_date > timezone.now():
+            sorted_groups = groups.order_by("users__count")
+            target_group = sorted_groups.first()
+            target_group.users.add(user)
+
+            target_group.save()
+
+        else:
+            first_group = groups.first()
+            first_group.users.add(user)
+            first_group.save()
+
+        user_course_access.save()
