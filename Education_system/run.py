14f36fb72eba8ccf832982fb142ef2853845890c
Wasil P <it.dzemidok@gmail.com>
2024-03-01 22:13:43 +0100

    Edit file run

--- Education_system/run.py
+++ Education_system/run.py
@@ -2,6 +2,7 @@ from Education_system.models import UserCourseAccess, Group
 
 from django.utils import timezone
 from django.db import transaction
+from django.db.models import Count
 
 
 def distribute_users_to_groups(user_course_access: UserCourseAccess):
@@ -10,23 +11,24 @@ def distribute_users_to_groups(user_course_access: UserCourseAccess):
     user = user_course_access.user
     groups = Group.objects.filter(course=course)
 
-    filtered_groups = Group.objects.filter(users__count__lt=groups.max_users).order_by('users__count')
+    filtered_groups = Group.objects.annotate(user_count=Count('users')).\
+        filter(user_count__lt=course.max_users).order_by('user_count')
 
     if not filtered_groups:
-        raise ValueError("Достигнуто максимальное количество пользователей во всех группах.")
+        last_group = groups.last()
+        new_group = Group.objects.create(
+            name=str(last_group.name) + "1",
+            course=course)
+        new_group.users.set([user])
 
+        user_course_access.save()
 
-    with transaction.atomic():
-        if course.start_date > timezone.now():
-            sorted_groups = groups.order_by("users__count")
+    if course.start_date > timezone.now():
+        with transaction.atomic():
+            sorted_groups = groups.annotate(user_count=Count('users')).order_by("user_count")
             target_group = sorted_groups.first()
             target_group.users.add(user)
 
             target_group.save()
 
-        else:
-            first_group = groups.first()
-            first_group.users.add(user)
-            first_group.save()
 
-        user_course_access.save()
